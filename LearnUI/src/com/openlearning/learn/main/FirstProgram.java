d79f2ce56fbd1ae7aa590225463da20aaff81cc1
pkrnavin <pkrnavin@gmail.com>
2018-08-21 12:21:02 +0530

    - To learn UI, of test to checking to try; projects `LearnUI`, `Test` adds in the repository, sample files added

---
+++ LearnUI/src/com/openlearning/learn/main/FirstProgram.java
@@ -0,0 +1,16 @@
+package com.openlearning.learn.main;
+
+/*
+ * below tries, FirstProgram in laptop eclipse in the project 
+ */
+public class FirstProgram {
+	
+	/**
+	 * main function, adds, to checking, runs tries 
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("FirstProgram in laptop eclipse in the project, `08-Jul-2018`");
+	}
+}
